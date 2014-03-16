5cee91a86444194a4cd7b561b5007983084131d4
Zach Latta <zach@zachlatta.com>
2014-03-16 01:42:51 -0700

    Use Martini and add basic functionality.

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,16 @@
-function addItem(text) {
+var socket = new WebSocket('ws://localhost:4000/socket');
+
+socket.onopen = function () {
+  console.log('Socket opened.');
+}
+
+socket.onmessage = function (msg) {
+  console.log('Message received.');
   docHeight = $(document).innerHeight() * 0.9;
   docWidth = $(document).innerWidth() * 0.9;
   textCol = '#' + Math.floor(Math.random() * 16777215).toString(16);
   textSize = Math.floor((Math.random() * 48) + 12);
   yPos = Math.floor((Math.random() * docHeight));
   xPos = Math.floor((Math.random() * docWidth));
-  $('<div class="wow" style="color:' + textCol + ';left:' + xPos + 'px;top:' + yPos + 'px;font-size:' + textSize + 'px;">' + text + '</div>').appendTo("body").fadeIn('fast').delay(4000).fadeOut('fast');
+  $('<div class="wow" style="color:' + textCol + ';left:' + xPos + 'px;top:' + yPos + 'px;font-size:' + textSize + 'px;">' + msg + '</div>').appendTo("body").fadeIn('fast').delay(4000).fadeOut('fast');
 }
--- server.go
+++ server.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/codegangsta/martini"
+	"github.com/gorilla/websocket"
 )
+
+var conns []*websocket.Conn
 
 func main() {
 	m := martini.Classic()
 
-	m.Use(martini.Static("public"))
+	m.Use(martini.Static("assets"))
 
-	m.Post(`/new_email`, HandleNewEmail)
+	m.Get("/socket", socketHandler)
+	m.Post("/new_mail", newMailHandler)
 
 	m.Run()
 }
 
-func HandleNewEmail(r *http.Request) int {
+func newMailHandler(w http.ResponseWriter, r *http.Request) int {
+	body := r.FormValue("text")
+	for _, conn := range conns {
+		go conn.WriteMessage(1, []byte(body))
+	}
+
 	return http.StatusOK
+}
+
+func socketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		log.Println(err)
+	}
+	conns = append(conns, conn)
 }
