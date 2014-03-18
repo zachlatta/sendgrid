d7e687135bc62ed4761f625746e3ce66dbf449d5
Zach Latta <zach@zachlatta.com>
2014-03-18 11:23:00 -0700

    Add server logic.

--- server.go
+++ server.go
@@ -21,7 +21,7 @@ func main() {
 	m.Run()
 }
 
-func newMailHandler(w http.ResponseWriter, r *http.Request) int {
+func newMailHandler(r *http.Request, log *log.Logger) int {
 	body := r.FormValue("text")
 	log.Println("Mail received: " + body)
 	for _, conn := range conns {
